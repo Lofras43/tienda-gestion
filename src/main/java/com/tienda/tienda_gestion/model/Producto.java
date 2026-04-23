75138f99d7efc86a819885f18d3510827e0b49c9
Lofras43 <lofras43@github.com>
2026-04-23 00:28:12 -0500

    fix: Cambiar a Spring Boot 2.7 y Java 11 para compatibilidad

--- src/main/java/com/tienda/tienda_gestion/model/Producto.java
+++ src/main/java/com/tienda/tienda_gestion/model/Producto.java
@@ -1,6 +1,6 @@
 package com.tienda.tienda_gestion.model;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
